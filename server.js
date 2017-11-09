4eda58966b208484d729da20c32c4edc46098e0f
Nandun Bandara <ntbandara3@hotmail.com>
2017-11-09 22:07:59 +0530

    test port

--- server.js
+++ server.js
@@ -6,8 +6,26 @@
 const express = require('express');
 const app = express();
 
+const DatabaseConnection = require('./lib/database');
+
 app.get('/', (req,res)=>{
     res.status(200).json({ message: "Welcome to the Sample REST API!"});
 });
 
-app.listen(process.env.PORT || 4201);
+app.listen(process.env.PORT || 4201, (err)=>{
+    if(err){
+        console.log('Could not start application!');
+    }else{
+        console.log('Server started!');
+    }
+});
+
+// let db = new DatabaseConnection();
+
+// let sequelize = db.getConnection();
+
+// sequelize.authenticate().then(()=>{
+//     console.log('Connection has been established!');
+// }).catch(err=>{
+//     console.log('Unable to connect to the database: ',err);
+// });
