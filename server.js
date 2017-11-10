6b2a5f3e941dd6d5ed7112c7d4a3d1716f829bc1
Nandun Bandara <ntbandara3@hotmail.com>
2017-11-11 01:58:25 +0530

    Database connection and configurations module

--- server.js
+++ server.js
@@ -6,23 +6,17 @@
 const express = require('express');
 const app = express();
 
-const mysql = require('mysql');
-
-const connection = mysql.createConnection({
-    host: 'cyborgs.cf0bzul2pkld.us-west-2.rds.amazonaws.com',
-    user: 'cyb_root',
-    password: 'pass123$',
-    database: 'sakila'
-});
-
-connection.connect();
+const Database = require('./lib/database');
 
 app.get('/', (req,res)=>{
     res.status(200).json({ message: "Welcome to the Sample REST API!" });
 });
 
+/*
+GET all actors
+ */
 app.get('/actors', (req,res)=>{
-    connection.query('SELECT * FROM actor', (err,rows, fields)=>{
+    Database.getConnection().query('SELECT * FROM actor', (err,rows, fields)=>{
         if(err){
             res.status(500).json({ success: false, message: "Could not retrieve actors!", error:err});
             return;
@@ -31,16 +25,19 @@ app.get('/actors', (req,res)=>{
     });
 });
 
+/*
+GET actor by given id
+ */
 app.get('/actors/:id', (req,res)=>{
     let query_string = "SELECT * FROM actor WHERE actor_id = '"+req.params.id+"'";
-    connection.query(query_string, (err,rows, fields)=>{
+    Database.getConnection().query(query_string, (err,rows, fields)=>{
         if(err){
             res.status(500).json({ success: false, message: "Could not retrieve actor!", error:err});
             return;
         }
         res.status(200).json({ success: true, data:rows });
     });
-})
+});
 
 app.listen(process.env.PORT || 4201, (err)=>{
     if(err){
